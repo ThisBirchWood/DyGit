2679cfc7fe0eb0ba7b61a79a4b1bf80772567eda
ThisBirchWood <dylanseandefaoite@gmail.com>
2024-10-06 11:22:39 +0100

    Added author attribute

--- commit.py
+++ commit.py
@@ -1,6 +1,7 @@
 import tree
 
 class Commit:
-    def __init__(self, tree: tree.Tree, message):
+    def __init__(self, tree: tree.Tree, message: str, author: str):
         self.root_tree = tree
         self.message = message
+        self.author = author

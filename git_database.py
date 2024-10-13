e31681a4be21e85370e4287d324989c78074646e
ThisBirchWood <dylanseandefaoite@gmail.com>
2024-10-13 12:22:50 +0100

    Added init method

--- git_database.py
+++ git_database.py
@@ -12,7 +12,28 @@ class GitDatabase:
         self.file_manager = FileManager(self.dir)
         self.config = ConfigReader()
 
-        self.object_directory = self.file_manager.join_paths(self.dir, self.config.get_object_directory_name())
-        self.object_database = ObjectDatabase(self.object_directory)
+        self.git_directory_path = self.config.get_git_directory_name()
+        self.branches_directory_path = self.file_manager.join_paths(self.git_directory_path, self.config.get_branches_directory_name())
+        self.object_directory_path = self.file_manager.join_paths(self.config.get_git_directory_name(), self.config.get_object_directory_name())
+        self.current_branch_file_path = self.file_manager.join_paths(self.git_directory_path, self.config.get_current_branch_file())
+        self.default_branch_file_path = self.file_manager.join_paths(self.branches_directory_path, self.config.get_default_branch_name())
+        self.ignore_file_name = self.config.get_gitignore_file()
 
-        self.current_branch = "main"
+        self.object_database = ObjectDatabase(self.file_manager.join_paths(self.dir, self.object_directory_path))
+
+
+    def init(self):
+        self.current_branch = self.config.get_default_branch_name()
+
+        self.file_manager.create_directory(self.git_directory_path)
+        self.file_manager.create_directory(self.branches_directory_path)
+        self.file_manager.create_directory(self.object_directory_path)
+        self.file_manager.create_file(self.current_branch_file_path, self.current_branch.encode())
+        self.file_manager.create_file(self.default_branch_file_path, b'')
+        self.file_manager.create_file(self.ignore_file_name, b'')
+
+
+
+if __name__ == "__main__":
+    git = GitDatabase(r"C:\Users\Dylan De Faoite\Documents\Chord Gen")
+    git.init()

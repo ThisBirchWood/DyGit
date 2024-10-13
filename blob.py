e09f91ffacd921c942e5cf37b2379243c1d3d4c5
ThisBirchWood <dylanseandefaoite@gmail.com>
2024-10-13 12:20:51 +0100

    Added type annotations for Blob

--- blob.py
+++ blob.py
@@ -1,7 +1,7 @@
 import hash
 
 class Blob:
-    def __init__(self, data):
+    def __init__(self, data: bytes):
         self.data = data
         self.hash = hash.sha1_hash(self.data)
         self.size = len(self.data)
